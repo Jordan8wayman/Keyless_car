2d0b85df62c9367d46b049e99216748723b6bff8
jordan8wayman <jordanwayman1998@gmail.co.uk>
2022-10-19 17:25:51 +0100

    EXERSISE 5
    2. create another function that does the same thing using function
    expression.

--- car.js
+++ car.js
@@ -11,3 +11,15 @@ function checkDriverAge(){
     }
 }
 
+var age = function(){
+    var age = prompt("What is your age?")
+    if (age < Number(18)) {
+        alert("sorry you are too young to drive this car. Powering off");
+    }
+    else if (age > Number(18)) {
+        alert("Powering On. Enjoy the ride!");
+    }
+    else {
+        alert("Congratulations on your first year of driving. Enjoy the ride!");
+    }
+}
