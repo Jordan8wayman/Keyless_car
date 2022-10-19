730486e6771856f8d14b8cdc1035efb832d6bb37
jordan8wayman <jordanwayman1998@gmail.co.uk>
2022-10-19 16:31:11 +0100

    KEYLESS CAR
    Completed the first keyless car excersise using the if, else and else if
    conditionals.

---
+++ car.js
@@ -0,0 +1,13 @@
+var age = prompt("how old are you?")
+
+if (age < 18) {
+    alert("sorry you are too young to drive this car. Powering off");
+}
+
+else if (age === 18) {
+    alert("Congratulations on your first year of driving. Enjoy the ride!");
+}
+
+else {
+    alert("Powering On. Enjoy the ride!");
+}
